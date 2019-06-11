9dad567bd58315a7754265ed39ad96e867b62712
Conor Haining <conor.haining@gmail.com>
2019-06-11 19:35:23 +0100

    When given the Lat and Long, return the closest stations
    8 stations are returned by default, this is changable.

--- src/services/StationInfo.ts
+++ src/services/StationInfo.ts
@@ -15,4 +15,28 @@ export class StationInfo {
         });
     }
 
+    findClosest(coordinates: LatLng, returnNumber: number = 8) {
+        return this.stations.map(station => {
+            const R = 6371; // Radius of the earth in km
+            const location = station.location;
+
+            const dLat = (location.latitude - coordinates.latitude) * (Math.PI / 180);
+            const dLong = (location.longitude - coordinates.longitude) * (Math.PI / 180);
+
+            const a = 
+                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(coordinates.latitude * (Math.PI / 180)) * Math.cos(location.latitude * (Math.PI / 180)) * 
+                Math.sin(dLong/2) * Math.sin(dLong/2)
+                ;
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+            const d = R * c; // Distance in km
+
+            station.distance = Number(d.toFixed(2));
+
+            return station;
+        }).sort(function(a, b) {
+            return a.distance - b.distance;
+        }).slice(0, 8);
+    }
+
 }
